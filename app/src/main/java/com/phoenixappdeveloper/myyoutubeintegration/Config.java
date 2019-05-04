6fb3773beb212fb17d2620fdd4bfd94515674471
sourabhsharmans0614 <sourabhsharmans0614@gmail.com>
2019-05-04 15:53:23 +0530

    First commit

---
+++ app/src/main/java/com/phoenixappdeveloper/myyoutubeintegration/Config.java
@@ -0,0 +1,9 @@
+package com.phoenixappdeveloper.myyoutubeintegration;
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "YOUR API KEY";
+}
